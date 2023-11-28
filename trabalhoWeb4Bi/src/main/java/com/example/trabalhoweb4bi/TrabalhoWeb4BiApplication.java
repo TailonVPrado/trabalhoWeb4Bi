f41f8f89a00572b8ef5dc1e875d54463a0d2338e
TailonVPrado <tailon.prado@edu.unipar.br>
2023-11-28 18:30:13 -0300

    Criação do projeto

---
+++ trabalhoWeb4Bi/src/main/java/com/example/trabalhoweb4bi/TrabalhoWeb4BiApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabalhoweb4bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoWeb4BiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoWeb4BiApplication.class, args);
+	}
+
+}
---
+++ trabalhoWeb4Bi/src/test/java/com/example/trabalhoweb4bi/TrabalhoWeb4BiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalhoweb4bi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoWeb4BiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
